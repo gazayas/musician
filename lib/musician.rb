08ae8db2994e53b18817a780fa86c663e0f49a48
gazayas <g-zayas@hotmail.com>
2016-09-16 15:13:59 +0900

    テストの追加

--- lib/musician.rb
+++ lib/musician.rb
@@ -2,7 +2,7 @@ Sharp = "♯"
 Flat = "♭"
 SHARP_CHORDS = ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"]
 FLAT_CHORDS = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"]
-ADDITIONS = ["dim7", "dim", "2", "sus4", "sus", "maj7", "maj", "m7", "m", "aug", "7", "6", "9", "11"]
+ADDITIONS = ["add2", "add9", "dim7", "dim", "sus4", "sus", "maj7", "maj", "m7", "m", "aug", "2", "6", "7", "9", "11"]
 
 require 'files/note'
 require 'files/chord'
--- test/musician_test.rb
+++ test/musician_test.rb
@@ -3,6 +3,109 @@ require 'musician'
 
 class KeyChangeTest < Minitest::Test
 
+  def test_change_to_sharp
+    note = Note.new "A#"
+    assert_equal "A♯", note.name
+  end
+
+  def test_change_to_flat
+    note = Note.new "Ab"
+    assert_equal "A♭", note.name
+  end
+
+  def test_song_method_returns_array_of_strings
+    song = Song.new("G", ["G", "Em", "C", "D"])
+    chord_array = song.chord_names
+    assert_equal ["G", "Em", "C", "D"], chord_array
+  end
+
+  def test_key_change_changes_key_as_well
+    key = "G"
+    chords = ["G", "Em", "C", "D"]
+    song = Song.new(key, chords)
+    new_key = "A"
+    song = song.key_change(new_key)
+    assert_equal new_key, song.key.name
+  end
+
+  # ADDITIONSに関するテスト
+  def test_add2
+    chord = Chord.new("Cadd2")
+    assert_equal "add2", chord.addition
+  end
+
+  def test_add9
+    chord = Chord.new("Cadd9")
+    assert_equal "add9", chord.addition
+  end
+
+  def test_dim7
+    chord = Chord.new("Cdim7")
+    assert_equal "dim7", chord.addition
+  end
+
+  def test_dim
+    chord = Chord.new("Cdim")
+    assert_equal "dim", chord.addition
+  end
+
+  def test_sus4
+    chord = Chord.new("Csus4")
+    assert_equal "sus4", chord.addition
+  end
+
+  def test_sus
+    chord = Chord.new("Csus")
+    assert_equal "sus", chord.addition
+  end
+
+  def test_maj7
+    chord = Chord.new("Cmaj7")
+    assert_equal "maj7", chord.addition
+  end
+
+  def test_maj
+    chord = Chord.new("Cmaj")
+    assert_equal "maj", chord.addition
+  end
+
+  def test_m7
+    chord = Chord.new("Cm7")
+    assert_equal "m7", chord.addition
+  end
+
+  def test_m
+    chord = Chord.new("Cm")
+    assert_equal "m", chord.addition
+  end
+
+  def test_aug
+    chord = Chord.new("Caug")
+    assert_equal "aug", chord.addition
+  end
+
+  def test_2
+    chord = Chord.new("C2")
+    assert_equal "2", chord.addition
+  end
+
+  def test_6
+    chord = Chord.new("C6")
+    assert_equal "6", chord.addition
+  end
+
+  def test_9
+    chord = Chord.new("C9")
+    assert_equal "9", chord.addition
+  end
+
+  def test_11
+    chord = Chord.new("C11")
+    assert_equal "11", chord.addition
+  end
+
+
+  # 転調に関するテスト
   def test_G_to_A
     key = "G"
     chords = ["G", "Em", "C", "D"]
@@ -21,19 +124,6 @@ class KeyChangeTest < Minitest::Test
     assert_equal ["G", "Em", "C", "D"], song.chord_names
   end
 
-  def test_song_method_returns_array
-    song = Song.new("G", ["G", "Em", "C", "D"])
-    chord_array = song.chord_names
-    assert_equal ["G", "Em", "C", "D"], chord_array
-  end
-
-  def test_key_change_changes_key_as_well
-    key = "G"
-    chords = ["G", "Em", "C", "D"]
-    song = Song.new(key, chords)
-    new_key = "A"
-    song = song.key_change(new_key)
-    assert_equal new_key, song.key.name
-  end
+  # 後はオプションのテストを書くこと（:sharp, :all_sharpとか）
 
 end
